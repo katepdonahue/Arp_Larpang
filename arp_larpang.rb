db71e0fc0beed7a10aea02515d0f1488f73606ae
Kate <katepdonahue@gmail.com>
2013-12-04 11:52:27 -0500

    Get capitalization to stick

--- arp_larpang.rb
+++ arp_larpang.rb
@@ -7,8 +7,21 @@ class String
   end
 
   def pig_latin
-    match = (/\A([^aeiouAEIOU]*)([aeiouyAEIOUY].*)/).match(self)
-    match[2] + match[1].downcase + "ay"
+    match = (/\A([Yy]?[^aeiouyAEIOUY]*)([aeiouyAEIOUY]*.*)/).match(self)
+    if match[1] == ""
+      "#{match[2]}way"
+    elsif match[1] == match[1].capitalize && match[2] != ""
+      "#{match[2].capitalize}#{match[1].downcase}ay"
+    else
+      "#{match[2]}#{match[1]}ay"
+    end
   end
+
+  def pig_latin_sentence
+    self.split(/\b/).each do |word|
+      word.pig_latin
+    end
+  end
+
 
 end
--- spec.rb
+++ spec.rb
@@ -11,22 +11,31 @@ describe String do
 
   end
 
+# think about what you want to do with capital letters
   describe "#pig_latin" do
 
     it "should take the first consonant off the beginning of the word and put it at the end followed by an ay" do
-      expect("Sarah".pig_latin).to eq("arahsay")
+      expect("Sarah".pig_latin).to eq("Arahsay")
     end
 
     it "should take all consonants in a row off the beginning of the word and put them at the end followed by an ay" do
-      expect("Shoshana".pig_latin).to eq("oshanashay")
+      expect("Shoshana".pig_latin).to eq("Oshanashay")
     end
 
     it "should not remove a vowel off the beginning of the word but still put an ay" do
-      expect("Emma".pig_latin).to eq("Emmaay")
+      expect("Emma".pig_latin).to eq("Emmaway")
+    end
+
+    it "should not break when you only give it consonants" do
+      expect("Shhhhhh".pig_latin).to eq("Shhhhhhay")
+    end
+
+    it "should not break when vowels are only y's" do
+      expect("rhythm".pig_latin).to eq("ythmrhay")
     end
 
     it "should work on a sentence" do
-      expect("Hello my name is Kate.").to eq("ellohay ymay amenay isay atekay.")
+      expect("Hello my name is Kate.".pig_latin).to eq("Ellohay ymay amenay isay atekay.")
     end
 
   end
